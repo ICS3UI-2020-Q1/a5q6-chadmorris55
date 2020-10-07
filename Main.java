9855a5b1b2ed9b64058a9900474fb8727a5cf4be
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-07 09:08:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * prints a box of stars with equal height and width to whatever number was given
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    //asks user for number and records it top use later
+    System.out.println("Please enter a number between 1 and 10");
+    int number = input.nextInt();
+    //prints out a star each time the count is belwo or equal to the given number
+     for (int countB = 1; countB <= number; countB++){
+     for (int countA = 1; countA <= number; countA++){
+      System.out.print("*");
+      }
+      System.out.println();
+      }
   }
 }
